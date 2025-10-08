567d9359655958edfa18d51dc130d0194d50b562
ggjk26 <conater@outlook.com>
2025-10-08 22:06:18 +0800

    Update __init__.py

--- embedded_utau/__init__.py
+++ embedded_utau/__init__.py
@@ -1,6 +1,6 @@
 # embedded_utau/__init__.py
 """
-PyUTAU - 基于Python的UTAU-like语音合成库
+PyUTAU - 基于Python的UTAU乐声合成插件
 """
 
 from .gui_component import PyUTAUComponent
@@ -12,7 +12,7 @@ from .ust_parser import USTParser
 from .library_adapter import LibraryAdapter  # 新增
 from .library_detector import LibraryDetector  # 新增
 
-__version__ = "0.0.4"  # 更新版本号
+__version__ = "0.0.4"  
 __all__ = [
     'PyUTAUComponent', 
     'SynthesisEngine', 
@@ -22,6 +22,7 @@ __all__ = [
     'Project',
     'ProjectSettings',
     'USTParser',
-    'LibraryAdapter',  # 新增
-    'LibraryDetector'  # 新增
+    'LibraryAdapter',  
+    'LibraryDetector'  
 ]
+
